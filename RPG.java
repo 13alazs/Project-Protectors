2ccfa54a7de9e4e65dad4cf773526a58d22f54c1
FodorAdam <80267342+FodorAdam@users.noreply.github.com>
2021-03-12 14:43:12 +0100

    Added 3 files

---
+++ RPG.java
@@ -0,0 +1,12 @@
+package com.mycompany.protectors;
+
+/*
+    Project Name: Project Protectors
+    Members: Árvai Balázs, Bíró Benjámin, Fodor Ádám, Zászlós Dorottya Beáta
+*/
+
+public class RPG {
+    public static void main(String[] args){
+        GameFrame frame = new GameFrame();
+    }
+}
