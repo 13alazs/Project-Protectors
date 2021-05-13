2d1393d5a267fd8563d74dce8dec4124082cc4fb
BiroBenjamin <dsgct9@inf.elte.hu>
2021-05-13 15:40:04 +0200

    Added more color to the tooltips

--- src/main/java/protectors/UITooltip.java
+++ src/main/java/protectors/UITooltip.java
@@ -1,51 +1,104 @@
 package protectors;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
 
 public class UITooltip extends JPanel {
     private JLabel tooltipText;
 
+    private String name;
+    private String resourceName;
+    private String cost;
+    private String damage;
+    private String target;
+    private String attackType;  //Fire, Ice, Blunt...
+    private String abilityModifier; //Random target, Adjacent..
+    private String resourcePerRound;
+    
     private String text;
 
     public UITooltip(int x, int y, Ability ability, String resource) {
         super();
 
         this.setBackground(new Color(0, 0, 0, 192));
-        this.setBounds(x, y, 260, 150);
-        this.setLayout(new FlowLayout(FlowLayout.CENTER));
-
-        if ("attack".equals(ability.getAbilityType())) {
-            text = "<html><font style='font-size: 15px;'>" + ability.getName() + "<br></font>Deal <font color='red'>"
-                    + ability.getAttackDamage() + "</font> damage to the targeted <font color='red'>enemy.</font>"
-                    + "<br>Cooldown: " + ability.getCooldown() + " turns ,<br>" + resource
-                    + " cost: <font color='yellow'>" + ability.getCost() + "</font>" + ",<br>target count: "
-                    + ability.getTargetCount() + "<html>";
-        } else if ("heal".equals(ability.getAbilityType())) {
-            text = "<html><font style='font-size: 15px;'>" + ability.getName()
-                    + "<br></font>Restore <font color='green'>" + Math.abs(ability.getAttackDamage())
-                    + "</font> health to the targeted <font color='green'>ally</font>." + "<br>Cooldown: "
-                    + ability.getCooldown() + " turns ,<br>" + resource + " cost: <font color='yellow'>"
-                    + ability.getCost() + "</font>" + ",<br>target count: " + ability.getTargetCount() + "<html>";
-        } else if ("stun".equals(ability.getAbilityType())) {
-            text = "<html><font style='font-size: 15px;'>" + ability.getName()
-                    + "<br></font>Stuns an <font color='red'>enemy</font> for <font color='red'>" + "</font> 1 rounds."
-                    + "<br>Cooldown: " + ability.getCooldown() + " turns ,<br>" + resource
-                    + " cost: <font color='yellow'>" + ability.getCost() + "</font>" + ",<br>target count: "
-                    + ability.getTargetCount() + "<html>";
-        } else if ("resurrect".equals(ability.getAbilityType())) {
-            text = "<html><font style='font-size: 15px;'>" + ability.getName()
-                    + "<br></font>Bring back an <font color='green'>ally</font> to life, or restore them <br> to full health."
-                    + "<br>Cooldown: " + ability.getCooldown() + " turns ,<br>" + resource
-                    + " cost: <font color='yellow'>" + ability.getCost() + "</font>" + ",<br>target count: "
-                    + ability.getTargetCount() + "<html>";
+        this.setBorder(new LineBorder(Color.BLACK, 3));
+        this.setBounds(x, y, 290, 100);
+        this.setLayout(new BorderLayout());
+        
+        name = "<font style='font-size: 15px'>" + ability.getName() + "</font><br>";
+        switch(resource){
+            case "mana":
+                resourceName = "<font color='rgb(15, 82, 186)'>" + resource + "</font>";
+                cost = "<font color='rgb(15, 82, 186)'>" + ability.getCost() + "</font>";
+                resourcePerRound = "+4 mana / turn.";
+                break;
+            case "focus":
+                resourceName = "<font color='rgb(240, 230, 140)'>" + resource + "</font>";
+                cost = "<font color='rgb(240, 230, 140)'>" + ability.getCost() + "</font>";
+                resourcePerRound = "+4 focus / turn for each enemy alive.";
+                break;
+            case "rage":
+                resourceName = "<font color='rgb(199, 0, 57)'>" + resource + "</font>";
+                cost = "<font color='rgb(199, 0, 57)'>" + ability.getCost() + "</font>";
+                resourcePerRound = "+4 rage / turn for each lost health points.";
+                break;
+            default:
+                break;
+        }
+        switch(ability.getAttackType()){
+            case "fire":
+                attackType = "<font color='rgb(255, 87, 51)'>" + ability.getAttackType() + "</font>";
+                break;
+            case "frost":
+                attackType = "<font color='rgb(137, 207, 240)'>" + ability.getAttackType() + "</font>";
+                break;
+            case "nature":
+                attackType = "<font color='rgb(138, 154, 91)'>" + ability.getAttackType() + "</font>";
+                break;
+            case "holy":
+                attackType = "<font color='rgb(225, 193, 110)'>" + ability.getAttackType() + "</font>";
+                break;
+            case "shadow":
+                attackType = "<font color='rgb(54, 69, 79)'>" + ability.getAttackType() + "</font>";
+                break;
+            default:
+                attackType = "<font color='rgb(131, 67, 51)'>" + ability.getAttackType() + "</font>";
+                break;
+        }
+        switch(ability.getAbilityModifier()){
+            case "normal":
+                abilityModifier = ability.getTargetCount() + " targeted ";
+                break;
+            case "random":
+                abilityModifier = ability.getTargetCount() + " random ";
+                break;
+        }
+        switch(ability.getAbilityType()){
+            case "resurrect":
+                text = "<html>" + name + "Resurrect " + abilityModifier + " ally or restore them <br> to full health.<br>" +
+                        "Cost: " + cost + " " + resourceName + ".<br>" + resourcePerRound + "</html>";
+                break;
+            case "stun":
+                text = "<html>" + name + "Stun " + abilityModifier + " enemy for one round.<br>" +
+                        "Cost: " + cost + " " + resourceName + ".<br>" + resourcePerRound + "</html>";
+                break;
+            case "heal":
+                text = "<html>" + name + "Restore " + "<font color='rgb(0, 163, 108)'>" + ability.getAttackDamage() * -1 + "</font>" + " health to " + abilityModifier + "ally.<br>" +
+                        "Cost: " + cost + " " + resourceName + ".<br>" + resourcePerRound + "</html>";
+                break;
+            default:
+                text = "<html>" + name + "Deal " + "<font color='rgb(154, 42, 42)'>" + ability.getAttackDamage() + "</font> " + attackType + " damage to " + abilityModifier + " enemy.<br>" +
+                        "Cost: " + cost + " " + resourceName + ".<br>" + resourcePerRound + "</html>";
+                break;
         }
 
-        tooltipText = new JLabel(text, JLabel.CENTER);
-        tooltipText.setBounds(5, 5, 250, 140);
+        tooltipText = new JLabel(text);
+        tooltipText.setBounds(10, 0, 290, 100);
         tooltipText.setForeground(Color.WHITE);
-        this.add(tooltipText);
+        this.add(tooltipText, BorderLayout.NORTH);
     }
 }
